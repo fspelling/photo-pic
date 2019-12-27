f1b8acc251a1a8cbc6accaac2f4bbeb64c3b7fd2
Fernando Spelling <fernando.spelling@linx.com.br>
2019-12-27 19:45:59 -0300

    .

--- pic-photo/src/App.vue
+++ pic-photo/src/App.vue
@@ -1,47 +1,10 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <ul class="lista-fotos">
-      <input class="filtro" type="search" v-on:input="filtro = $event.target.value" placeholder="pesquise aqui" />
-      <li class="lista-fotos-item" v-for="foto of fotosFilter">
-        <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-        </meu-painel>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Painel.vue";
-
 export default {
-  components: {
-    "meu-painel": Painel
-  },
-  data() {
-    return {
-      titulo: "Pic-Photo",
-      fotos: [],
-      filtro: ''
-    };
-  },
-  computed: {
-    fotosFilter() {
-      if (this.filtro) {
-        let regex = new RegExp(this.filtro, 'i');
-        return this.fotos.filter(foto => regex.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => (this.fotos = fotos), error => console.error(error));
-  }
 };
 </script>
 
@@ -50,26 +13,5 @@ export default {
   width: 96%;
   margin: 0 auto;
   font-family: Helvetica, sans-serif;
-}
-
-.centralizado {
-  text-align: center;
-}
-
-.lista-fotos {
-  list-style: none;
-}
-
-.lista-fotos-item {
-  display: inline-block;
-}
-
-.imagem-responsiva {
-  width: 100%;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
 }
 </style>
---
+++ pic-photo/src/components/cadastro/Cadastro.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="centralizado">Cadastro</h2>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scope>
+.centralizado {
+  text-align: center;
+}
+</style>
